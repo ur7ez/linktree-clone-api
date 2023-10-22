98b4ffa00d6f2fa1466f455667c81a809012c0f0
ur7ez <ur7ez@ukr.net>
2023-10-22 21:48:10 +0300

    define API routes for user image, link image and links management

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\Api\LinkController;
+use App\Http\Controllers\Api\LinkImageController;
 use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\UserImageController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,4 +20,13 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(['auth:sanctum'])->group(function () {
     Route::get('users', [UserController::class, 'index']);
     Route::patch('users/{user}', [UserController::class, 'update']);
+
+    Route::post('user-image', [UserImageController::class, 'store']);
+
+    Route::get('links', [LinkController::class, 'index']);
+    Route::post('links', [LinkController::class, 'store']);
+    Route::patch('links/{link}', [LinkController::class, 'update']);
+    Route::delete('links/{link}', [LinkController::class, 'destroy']);
+
+    Route::post('link-image', [LinkImageController::class, 'store']);
 });
