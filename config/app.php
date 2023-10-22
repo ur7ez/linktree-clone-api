645d1956fd53ab2a3b4772555f4b8e446c8a6e7d
ur7ez <ur7ez@ukr.net>
2023-10-22 21:44:09 +0300

    config for intervention/image package

--- config/app.php
+++ config/app.php
@@ -161,7 +161,7 @@ return [
         /*
          * Package Service Providers...
          */
-
+        Intervention\Image\ImageServiceProvider::class,
         /*
          * Application Service Providers...
          */
@@ -184,6 +184,7 @@ return [
     */
 
     'aliases' => Facade::defaultAliases()->merge([
+        'Image' => Intervention\Image\Facades\Image::class,
         // 'Example' => App\Facades\Example::class,
     ])->toArray(),
 
