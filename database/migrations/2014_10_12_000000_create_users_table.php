68ec1df75d0b23a5d14f7abf2b18edce4a89b969
ur7ez <ur7ez@ukr.net>
2023-10-22 22:43:32 +0300

    add theme_id to users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,12 +13,15 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+
+            $table->integer('theme_id');
             $table->string('name');
             $table->string('bio')->nullable();
             $table->text('image')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+
             $table->rememberToken();
             $table->timestamps();
         });
