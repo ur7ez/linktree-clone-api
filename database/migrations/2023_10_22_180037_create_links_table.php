51a7d983001d32a8a78e827cf674ea5c55673d0a
ur7ez <ur7ez@ukr.net>
2023-10-22 22:45:55 +0300

    add user_id field to links table

--- database/migrations/2023_10_22_180037_create_links_table.php
+++ database/migrations/2023_10_22_180037_create_links_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('links', function (Blueprint $table) {
             $table->id();
 
+            $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')
                 ->references('id')
                 ->on('users')
